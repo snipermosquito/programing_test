485affa99b9bde55f0e390aba4cbb68789646b50
snipermosquito <87172265+snipermosquito@users.noreply.github.com>
2023-05-29 06:51:29 +0900

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,1 +1,29 @@
-
+def def_var():
+  global log
+  log = "log.txt"
+  global list
+  list = []
+  global time_list
+  time_list = []
+  global ol_time_list
+  ol_time_list = []
+  global sn_time_list
+  sn_time_list = []
+  global sn_list
+  sn_list = []
+  global dic
+  dic = {}
+  global toc_dic
+  toc_dic = {}
+  global c_dic
+  c_dic = {}
+  global ol_dic
+  ol_dic = {}
+  global olc_dic
+  olc_dic = {}
+  global sn_dic
+  sn_dic = {}
+  global num
+  num = 0
+  global sn_num
+  sn_num = 0
