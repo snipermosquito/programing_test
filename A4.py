ac0963133e267ed24ea60720716e7c0ee4188ed2
snipermosquito <87172265+snipermosquito@users.noreply.github.com>
2023-05-30 09:38:44 +0900

    rewrite A4.py

--- A4.py
+++ A4.py
@@ -1,6 +1,6 @@
 import sys
-import math
 import utils
+import math
 
 if len(sys.argv) == 5:
     output = sys.argv[4]
@@ -13,53 +13,29 @@ log = "log.txt"
 list = []
 time_list = []
 ol_time_list = []
-sn_time_list = []
-sn_list = []
 dic = {}
 toc_dic = {}
 c_dic = {}
 ol_dic = {}
 olc_dic = {}
-sn_dic = {}
 num = 0
-sn_num = 0
 with open(log,'r') as f:
     lines = f.readlines()
 for line in lines:
     if line.isspace():
         break
-    sn = ""
-    npf = int(line.split(",")[1].split("/")[1])
-    for i in range(0,math.ceil(npf/8)):
-        sn = (sn + "." +  line.split(",")[1].split(".")[i]).strip(".")
-    if not line.split(",")[1] in list:
-        if not sn in sn_list:
-            sn_list.append(sn)
-            sn_dic[sn] = "No" + str(sn_num)
-            exec("{} = False".format(sn_dic[sn] + "_check"))
-            exec("{} = []".format(sn_dic[sn]))
-            sn_num += 1
+    elif not line.split(",")[1] in list:
         list.append(line.split(",")[1])
-        dic[line.split(",")[1]] = sn_dic[sn] + "_" + str(num)
-        toc_dic[line.split(",")[1]] = sn_dic[sn] + str(num) + "_TOCheck"
-        c_dic[line.split(",")[1]] = "c_" + sn_dic[sn] + "_" + str(num)
-        ol_dic[line.split(",")[1]] = "ol_" + sn_dic[sn] + str(num)
-        olc_dic[line.split(",")[1]] = sn_dic[sn] + str(num) + "_OLCheck"
+        dic[line.split(",")[1]] = "No" + str(num)
+        toc_dic[line.split(",")[1]] = "No" + str(num) + "_TOCheck"
+        c_dic[line.split(",")[1]] = "c_No" + str(num)
+        ol_dic[line.split(",")[1]] = "ol_No" + str(num)
+        olc_dic[line.split(",")[1]] = "No" + str(num) + "_OLCheck"
         exec("{} = False".format(toc_dic[line.split(",")[1]]))
         exec("{} = 0".format(c_dic[line.split(",")[1]]))
         exec("{} = []".format(ol_dic[line.split(",")[1]]))
         exec("{} = False".format(olc_dic[line.split(",")[1]]))
-        exec("tmp_list = {}".format(sn_dic[sn]))
-        if not dic[line.split(",")[1]] in tmp_list:
-            exec("{}.append(dic[line.split(',')[1]])".format(sn_dic[sn]))
         num += 1
-for line in lines:
-    if line.isspace():
-        break
-    sn = ""
-    npf = int(line.split(",")[1].split("/")[1])
-    for i in range(0,math.ceil(npf/8)):
-        sn = (sn + "." +  line.split(",")[1].split(".")[i]).strip(".")
     exec("var = {}".format(toc_dic[line.split(",")[1]]))
     if var:
         if line.split(",")[2].strip() != "-":
@@ -69,36 +45,13 @@ for line in lines:
             exec("c_var = {}".format(c_dic[line.split(",")[1]]))
             if c_var >= N:
                 time_list.append([line.split(",")[1],recov_time,dt])
-                exec("{} = 0".format(c_dic[line.split(",")[1]]))
+            exec("{} = 0".format(c_dic[line.split(",")[1]]))
         else:
             exec("{} += 1".format(c_dic[line.split(",")[1]]))
     elif line.split(",")[2].strip() == "-":
         exec("{} = True".format(toc_dic[line.split(",")[1]]))
         exec("{} = line.split(',')[0]".format(dic[line.split(',')[1]] + '_time'))
         exec("{} = 1".format(c_dic[line.split(",")[1]]))
-    exec("tmp_list = {}".format(sn_dic[sn]))
-    exec("sn_E_check = {}".format(sn_dic[sn] + "_check"))
-    if sn_E_check:
-        if line.split(",")[2].strip() != "-":
-            exec("{} = False".format(sn_dic[sn] + "_check"))
-            sn_max = 0
-            for i in tmp_list:
-                i += "_time"
-                exec("sn_tmp_time = int({})".format(i))
-                if sn_max < sn_tmp_time:
-                        sn_max = sn_tmp_time
-            recov_time = str(sn_max) + '-' + str(line.split(',')[0])
-            dt = utils.time_calc(recov_time)
-            sn_time_list.append([sn,recov_time,dt])
-    else:
-        for i in tmp_list:
-            i = "c_" + i
-            exec("j = {}".format(i))
-            if j >= m:
-                exec("{} = True".format(sn_dic[sn] + "_check"))
-            else:
-                exec("{} = False".format(sn_dic[sn] + "_check"))
-                break
     exec("{}.append([line.split(',')[0],line.split(',')[2].strip()])".format(ol_dic[line.split(',')[1]]))
     exec("length = len({})".format(ol_dic[line.split(',')[1]]))
     if length == m:
@@ -124,7 +77,6 @@ for line in lines:
                     dt = utils.time_calc(ol_recov_time)
                     ol_time_list.append([line.split(",")[1],ol_recov_time,dt])
         exec("{}.pop(0)".format(ol_dic[line.split(',')[1]]))
-
 for i in list:
     exec("var = {}".format(toc_dic[i]))
     exec("c_var = {}".format(c_dic[i]))
@@ -135,18 +87,90 @@ for i in list:
     if check:
         exec("ol_recov_time = {} + '-'".format(dic[i] + "_ol_time"))
         ol_time_list.append([i,ol_recov_time])
-    exec("tmp_list = {}".format(sn_dic[sn]))
-    exec("sn_E_check = {}".format(sn_dic[sn] + "_check"))
-    if sn_E_check:
-        exec("{} = False".format(sn_dic[sn] + "_check"))
-        sn_max = 0
-        for i in tmp_list:
-            i += "_time"
-            exec("sn_tmp_time = int({})".format(i))
-            if sn_max < sn_tmp_time:
-                sn_max = sn_tmp_time
-        recov_time = str(sn_max) + '-'
-        sn_time_list.append([sn,recov_time])
+    
+list = []
+new_lines = []
+sn_time_list = []
+sn_list = []
+dic = {}
+sn_dic = {}
+num = 0
+sn_num = 0
+with open(log,'r') as f:
+    lines = f.readlines()
+for line in lines:
+    if line.isspace():
+        break
+    sn = ""
+    npf = int(line.split(",")[1].split("/")[1])
+    for i in range(0,math.ceil(npf/8)):
+        sn = (sn + "." +  line.split(",")[1].split(".")[i]).strip(".")
+    if not line.split(",")[1] in list:
+        if not sn in sn_list:
+            sn_list.append(sn)
+            sn_dic[sn] = "No" + str(sn_num)
+            exec("{} = False".format(sn_dic[sn] + "_check"))
+            exec("{} = []".format(sn_dic[sn]))
+            sn_num += 1
+        list.append(line.split(",")[1])
+        dic[line.split(",")[1]] = sn_dic[sn] + "_" + str(num)
+        exec("{} = False".format(dic[line.split(",")[1]] + "_check"))
+        exec("tmp_list = {}".format(sn_dic[sn]))
+        if not dic[line.split(",")[1]] in tmp_list:
+            exec("{}.append(dic[line.split(',')[1]])".format(sn_dic[sn]))
+        num += 1
+for line in time_list:
+    sv = line[0]
+    fr = line[1].split("-")[0]
+    if line[1].split("-")[1] != "":
+        unt = line[1].split("-")[1]
+    else:
+        unt = "99999999999999"
+    new_lines.append([sv,fr,unt])
+new_lines = sorted(new_lines, key=lambda x: x[1])
+for line in new_lines:
+    sn = ""
+    npf = int(line[0].split("/")[1])
+    for i in range(0,math.ceil(npf/8)):
+        sn = (sn + "." +  line[0].split(".")[i]).strip(".")
+    now = int(line[1])
+    for svs in list:
+        sv_No = dic[svs]
+        exec("check = {}".format(sv_No + "_check"))
+        if check:
+            exec("end = {}".format(sv_No + "_end"))
+            if int(now) >= int(end):
+                exec("{} = False".format(sv_No + "_check"))
+    sv_No = dic[line[0]]
+    exec("{} = int(line[1])".format(sv_No + "_start"))
+    exec("{} = int(line[2])".format(sv_No + "_end"))
+    exec("{} = True".format(sv_No + "_check"))
+    sn_No = sn_dic[sn]
+    sn_check = True
+    exec("tmp_list = {}".format(sn_No))
+    for svs in tmp_list:
+        exec("check = {}".format(svs + "_check"))
+        if not check:
+            sn_check = False
+    if sn_check:
+        max = 0
+        min = 99999999999999
+        for svs in tmp_list:
+            exec("start = {}".format(svs + "_start"))
+            exec("end = {}".format(svs + "_end"))
+            if start > max:
+                max = start
+            if end < min:
+                min = end
+                end_check = svs
+        exec("{} = False".format(end_check + "_check"))
+        if min == 99999999999999:
+            max_min = str(max) + "-"
+            sn_time_list.append([sn,max-min])
+        else:
+            max_min = str(max) + "-" + str(min)
+            dt = utils.time_calc(max_min)
+            sn_time_list.append([sn,max_min,dt])
 
 if output == "":
     print("[timeout]")
